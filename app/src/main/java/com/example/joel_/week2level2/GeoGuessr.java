5a84d1550dea5810ec32c9a084ae5974746bb5c2
Jmaster2710 <joel_kroon@hotmail.com>
2018-09-10 14:58:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/joel_/week2level2/GeoGuessr.java
@@ -0,0 +1,13 @@
+package com.example.joel_.week2level2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeoGuessr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geo_guessr);
+    }
+}
