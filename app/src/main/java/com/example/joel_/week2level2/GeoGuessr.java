59a294215127c3cabf71e7bc3515155e250d6551
Jmaster2710 <joel_kroon@hotmail.com>
2018-09-19 21:00:21 +0200

    Added Swiping Functionality

--- app/src/main/java/com/example/joel_/week2level2/GeoGuessr.java
+++ app/src/main/java/com/example/joel_/week2level2/GeoGuessr.java
@@ -2,9 +2,11 @@ package com.example.joel_.week2level2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,12 +32,49 @@ public class GeoGuessr extends AppCompatActivity {
 
         //Assigning the layout manager.
         RecyclerView mGeoRecyclerView = findViewById(R.id.recyclerView);
-
-        RecyclerView.LayoutManager mLayoutManager = new StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 1);
 
         mGeoRecyclerView.setLayoutManager(mLayoutManager);
         GeoObjectAdapter mAdapter = new GeoObjectAdapter(this, mGeoObjects);
         mGeoRecyclerView.setAdapter(mAdapter);
 
+
+
+/*
+Add a touch helper to the RecyclerView to recognize when a user swipes to delete a list entry.
+An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
+and uses callbacks to signal when a user is performing these actions.
+*/
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback =
+
+                new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+                    @Override
+
+                    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder
+                            target) {
+
+                        return false;
+
+                    }
+
+                    //Called when a user swipes left or right on a ViewHolder
+
+                    @Override
+
+                    public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+
+                        //Get the index corresponding to the selected position
+
+                        int position = (viewHolder.getAdapterPosition());
+                        //mReminders.remove(position);
+                        //mAdapter.notifyItemRemoved(position);
+                    }
+
+                };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(mGeoRecyclerView);
     }
 }
