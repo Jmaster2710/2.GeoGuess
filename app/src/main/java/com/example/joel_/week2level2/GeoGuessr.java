a724bf5415ad39ec60b197020f73b8e2a9f409fe
Jmaster2710 <joel_kroon@hotmail.com>
2018-09-19 20:17:25 +0200

    Finished implementing the code from Level 1

--- app/src/main/java/com/example/joel_/week2level2/GeoGuessr.java
+++ app/src/main/java/com/example/joel_/week2level2/GeoGuessr.java
@@ -2,6 +2,12 @@ package com.example.joel_.week2level2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GeoGuessr extends AppCompatActivity {
 
@@ -9,5 +15,27 @@ public class GeoGuessr extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo_guessr);
+
+        List<GeoObject> mGeoObjects = new ArrayList<>();
+
+        for (int i = 0; i < GeoObject.PRE_DEFINED_GEO_OBJECT_NAMES.length; i++) {
+
+            mGeoObjects.add(
+                    new GeoObject(
+                            GeoObject.PRE_DEFINED_GEO_OBJECT_NAMES[i],
+                            GeoObject.PRE_DEFINED_GEO_OBJECT_IMAGE_IDS[i],
+                            GeoObject.PRE_DEFINED_GEO_OBJECT_EUROPE[i])
+            );
+        }
+
+        //Assigning the layout manager.
+        RecyclerView mGeoRecyclerView = findViewById(R.id.recyclerView);
+
+        RecyclerView.LayoutManager mLayoutManager = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
+
+        mGeoRecyclerView.setLayoutManager(mLayoutManager);
+        GeoObjectAdapter mAdapter = new GeoObjectAdapter(this, mGeoObjects);
+        mGeoRecyclerView.setAdapter(mAdapter);
+
     }
 }
--- app/src/main/java/com/example/joel_/week2level2/GeoObject.java
+++ app/src/main/java/com/example/joel_/week2level2/GeoObject.java
@@ -5,6 +5,46 @@ public class GeoObject {
     private int mGeoImageName;
     private boolean mIsEurope;
 
+
+    public static final String[] PRE_DEFINED_GEO_OBJECT_NAMES = {
+
+            "Denmark",
+            "Canada",
+            "Bangladesh",
+            "Kazachstan",
+            "Colombia",
+            "Poland",
+            "Malta",
+            "Thailand"
+
+    };
+
+    public static final int[] PRE_DEFINED_GEO_OBJECT_IMAGE_IDS = {
+
+            R.drawable.img1_yes_denmark,
+            R.drawable.img2_no_canada,
+            R.drawable.img3_no_bangladesh,
+            R.drawable.img4_yes_kazachstan,
+            R.drawable.img5_no_colombia,
+            R.drawable.img6_yes_poland,
+            R.drawable.img7_yes_malta,
+            R.drawable.img8_no_thailand
+    };
+
+    public static final boolean[] PRE_DEFINED_GEO_OBJECT_EUROPE = {
+
+            true,
+            false,
+            false,
+            true,
+            false,
+            true,
+            true,
+            false
+    };
+
+
+
     public GeoObject(String mGeoName, int mGeoImageName, boolean mIsEurope) {
         this.mGeoName = mGeoName;
         this.mGeoImageName = mGeoImageName;
@@ -28,15 +68,15 @@ public class GeoObject {
     }
 
     //Setters
-    public void setmGeoName(String mGeoName)
-    {
-        this.mGeoName = mGeoName;
-    }
     public void setmGeoImageName(int mGeoImageName)
     {
         this.mGeoImageName = mGeoImageName;
     }
-    public void setmIsEurope(boolean mIsEurope)
+    public void setmIsEurope(String mGeoName)
+    {
+        this.mGeoName = mGeoName;
+    }
+    public void setmGeoName(boolean mIsEurope)
     {
         this.mIsEurope = mIsEurope;
     }
