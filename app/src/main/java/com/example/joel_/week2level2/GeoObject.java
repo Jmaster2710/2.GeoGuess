e12151de77993088b25b000f7f60e390d8d4df2f
Jmaster2710 <joel_kroon@hotmail.com>
2018-09-19 19:38:22 +0200

    Created the getters and setters for the GeoObject Class

--- app/src/main/java/com/example/joel_/week2level2/GeoObject.java
+++ app/src/main/java/com/example/joel_/week2level2/GeoObject.java
@@ -1,4 +1,43 @@
 package com.example.joel_.week2level2;
 
 public class GeoObject {
+    private String mGeoName;
+    private int mGeoImageName;
+    private boolean mIsEurope;
+
+    public GeoObject(String mGeoName, int mGeoImageName, boolean mIsEurope) {
+        this.mGeoName = mGeoName;
+        this.mGeoImageName = mGeoImageName;
+        this.mIsEurope = mIsEurope;
+    }
+
+    //Getters
+    public String getmGeoName()
+    {
+        return mGeoName;
+    }
+
+    public int getmGeoImageName()
+    {
+        return mGeoImageName;
+    }
+
+    public boolean getmIsEurope()
+    {
+        return mIsEurope;
+    }
+
+    //Setters
+    public void setmGeoName(String mGeoName)
+    {
+        this.mGeoName = mGeoName;
+    }
+    public void setmGeoImageName(int mGeoImageName)
+    {
+        this.mGeoImageName = mGeoImageName;
+    }
+    public void setmIsEurope(boolean mIsEurope)
+    {
+        this.mIsEurope = mIsEurope;
+    }
 }
